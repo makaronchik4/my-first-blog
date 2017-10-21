516fb6fa34a000ea44b2c0ce0a2a7ce547d0ac05
makaronchik4 <amakarova1993@gmail.com>
2017-10-21 17:17:09 +0300

    Added view and template for detailed blog post as well as CSS for the site

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image_url = models.CharField(max_length=250)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
